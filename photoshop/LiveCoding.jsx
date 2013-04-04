b8f3f2d6eedd28da1dd8ad5bf4ada62a516cb93e
dderaedt <dderaedt@adobe.com>
2013-04-04 10:22:41 +0200

    Fixed EaselJS export

--- photoshop/LiveCoding.jsx
+++
@@ -1,1 +0,0 @@
-﻿//#target photoshop#include "../common/Utils.jsx" function main() {	if ( app.documents.length > 0 ) {				var doc = app.activeDocument;					var l = doc.layers[0];					log(l.kind );		}}main();

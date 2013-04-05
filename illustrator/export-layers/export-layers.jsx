20ac85448f1f3fd8564ec7ab0250e3a8e77d2ea0
dderaedt <dderaedt@adobe.com>
2013-04-05 16:17:13 +0200

    Refactored data extraction, layer rasterization and output

--- illustrator/export-layers/export-layers.jsx
+++ illustrator/export-layers/export-layers.jsx
@@ -1,32 +1,1 @@
-﻿#include "../LayerExporter.jsx"
-
-#target illustrator
-
-function main() {
-
-	if ( app.documents.length > 0 ) {
-		
-		var doc = app.activeDocument;
-		var docFile = doc.fullName;
-
-		if(!doc.saved){
-			Window.alert("This script needs to modify your document. Please save it before running this script.");
-			return;
-		}
-
-		var destFolder = Folder.selectDialog ("Select Destination Folder");
-		if(!destFolder) return;
-				
-		LayerExporter.exportLayers(doc, destFolder, ExportType.SVG);
-
-
-		// Close and reopen original
-		// necessary since the script needs to modify the original file
-		doc.close(SaveOptions.DONOTSAVECHANGES);
-		doc = null;
-		app.open(docFile);
-
-	}
-
-}
-main();
+﻿#include "../LayerExporter.jsx"#include "../LayerDataExtractor.jsx"#target illustratorfunction exportLayers(exportType) {	if ( app.documents.length > 0 ) {				var doc = app.activeDocument;		var docFile = doc.fullName;		if(!doc.saved){			Window.alert("This script needs to modify your document. Please save it before running this script.");			return;		}		var destFolder = Folder.selectDialog ("Select Destination Folder");		if(!destFolder) return;						var ldata = LayerDataExtractor.exportLayersData(doc, exportType);		$.writeln(ldata);				LayerExporter.exportLayers(doc, destFolder, ExportType.SVG);		// Close and reopen original		// necessary since the script needs to modify the original file		doc.close(SaveOptions.DONOTSAVECHANGES);		doc = null;		app.open(docFile);	}}exportLayers(ExportType.SVG);
--- illustrator/export2EdgeAnimate/export2AnimateTest.jsx
+++ illustrator/export2EdgeAnimate/export2AnimateTest.jsx
@@ -1,1 +1,1 @@
-﻿// This file is intended to test the EdgeAnimate exporter // outside the panel context#include "export2Animate.jsx"var useSVG = false;doExport(useSVG);
+﻿// This file is intended to test the EdgeAnimate exporter // outside the panel context#include "export2Animate.jsx"var useSVG = true;doExport(useSVG);

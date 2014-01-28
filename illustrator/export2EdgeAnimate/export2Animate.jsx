52cb1282a63e2c232452613a6a78413b0af7fe24
dderaedt <dderaedt@adobe.com>
2014-01-28 15:48:28 +0100

    updated paths

--- illustrator/export2EdgeAnimate/export2Animate.jsx
+++ illustrator/export2EdgeAnimate/export2Animate.jsx
@@ -1,1 +1,48 @@
-﻿#include "../../edgeanimate/EdgeAnimateImporter.jsx"#include "../LayerDataExtractor.jsx"#include "../LayerExporter.jsx"#target illustratorfunction doExport(useSVG) {	if ( app.documents.length > 0 ) {				var doc = app.activeDocument;		var docFile = doc.fullName;		if(!doc.saved){			Window.alert("This script needs to modify your document. Please save it before running this script.");			return;		}		var anFile = File.openDialog ("Select the .an file of the destination Edge Animate project.");			if(!anFile) return;						var exportType = useSVG ? ExportType.SVG : ExportType.PNG24;						var itemsData = LayerDataExtractor.exportLayersData(doc, exportType);		$.writeln(itemsData);				var destFolder = anFile.parent;		var imgFolder = new Folder(destFolder.path +"/"+ destFolder.name + "/images/");		if(!imgFolder.exists) imgFolder.create();		var dest = imgFolder.absoluteURI+"/";		LayerExporter.exportLayers(doc, imgFolder, exportType);				EdgeAnimateImporter.doImport(anFile, itemsData);				// Close and reopen original		// (necessary since the script needs to modify the original file)			doc.close(SaveOptions.DONOTSAVECHANGES);		doc = null;		app.open(docFile);	}}
+#include "../../edgeanimate/EdgeAnimateImporter.jsx"
+#include "../export-layers-v1/LayerDataExtractor.jsx"
+#include "../export-layers-v1/LayerExporter.jsx"
+
+#target illustrator
+
+function doExport(useSVG) {
+
+	if ( app.documents.length > 0 ) {
+		
+		var doc = app.activeDocument;
+		var docFile = doc.fullName;
+
+		if(!doc.saved){
+			Window.alert("This script needs to modify your document. Please save it before running this script.");
+			return;
+		}
+
+		var anFile = File.openDialog ("Select the .an file of the destination Edge Animate project.");	
+		if(!anFile) return;
+		
+		
+		var exportType = useSVG ? ExportType.SVG : ExportType.PNG24;
+		
+		
+		var itemsData = LayerDataExtractor.exportLayersData(doc, exportType);
+		$.writeln(itemsData);		
+
+		var destFolder = anFile.parent;
+		var imgFolder = new Folder(destFolder.path +"/"+ destFolder.name + "/images/");
+		if(!imgFolder.exists) imgFolder.create();
+		var dest = imgFolder.absoluteURI+"/";
+
+		LayerExporter.exportLayers(doc, imgFolder, exportType);
+		
+		EdgeAnimateImporter.doImport(anFile, itemsData);
+		
+
+		// Close and reopen original
+		// (necessary since the script needs to modify the original file)	
+		doc.close(SaveOptions.DONOTSAVECHANGES);
+		doc = null;
+		app.open(docFile);
+
+	}
+
+}
+doExport(true);
